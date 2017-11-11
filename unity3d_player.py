9ba216a27211b23be53698c86e1bed7de9b532bc
williamd4112 <williamd4112@hotmail.com>
2017-11-12 06:26:48 +0800

    add comp reward

--- unity3d_player.py
+++ unity3d_player.py
@@ -67,8 +67,10 @@ class Unity3DPlayer(RLEnvironment):
             self._ob = self._process_state(env_info.observations[0][0])
             reward = env_info.rewards[0]
             done = env_info.local_done[0]
-
+            
             if done:
+                if reward > 0:
+                    reward = 1.0 - self.rwd_counter.sum
                 break            
         self.rwd_counter.feed(reward)
         if done:
